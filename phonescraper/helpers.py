03a484e7197ab1403864294a23cf895b475cfab7
thomasgruebl <thomasgruebl@gmx.at>
2021-06-21 21:56:12 +0200

    PS-16 bug fix

--- phonescraper/helpers.py
+++ phonescraper/helpers.py
@@ -118,6 +118,8 @@ class Email:
                f"The following phone numbers have been recently added:\n\n{tidy_phone_list}"
         message = f"{subject}\n\n{body}"
         msg = MIMEText(message)
+        del tidy_phone_list
+        
         return msg
 
     def __send_email(self, connection, msg):
